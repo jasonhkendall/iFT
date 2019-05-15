8bb1fa8f0e0da586ea6f6564b2342e35845bff80
Jason <jasonhkendall@gmail.com>
2019-05-15 13:36:08 -0400

    1.1

---
+++ iFT/iFT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iFT
+//
+//  Created by Jason Howard Kendall on 5/14/19.
+//  Copyright © 2019 JHK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
