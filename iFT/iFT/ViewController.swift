31695eb9232fff2141473384aa33e2aad4a713ef
Jason <jasonhkendall@gmail.com>
2019-05-16 13:46:33 -0400

    update

--- iFT/iFT/ViewController.swift
+++ iFT/iFT/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FirebaseUI
+import Firebase
+
 
 class ViewController: UIViewController {
 
@@ -14,7 +17,47 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+    
+    // Get default auth ui
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        
+        // Check that it isn't nil
+        guard authUI != nil else {
+            // Log the rror
+            return
+        }
+        
+        // Set self as the delegate
+        authUI?.delegate = self
+        authUI?.providers = [FUIEmailAuth()]
+        
+        // Get a reference to the auth UI view controller
+        let authViewController = authUI!.authViewController()
+        
+        // Show it
+        present(authViewController, animated: true, completion: nil)
+    }
+        
+        
+    }
+    
 
-
-}
+    
+    extension ViewController: FUIAuthDelegate {
+        
+        func authUI(_ authUI: FUIAuth, didSignInWith authDataResult:
+            AuthDataResult?, error: Error?) {
+            
+            // Check for error
+            guard error == nil else {
+                return
+            }
+            
+            // authDataResult?.user.uid
+            performSegue(withIdentifier: "goHome", sender: self)
+        }
+    }
 
