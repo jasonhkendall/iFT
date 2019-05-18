6643ac7b2a7698ae3d4b124b9af60be80e864b40
Jason <jasonhkendall@gmail.com>
2019-05-18 12:19:42 -0400

    Add VCs
    Create classes and added sign-in / sign-out view controllers

--- iFT/iFT/ViewController.swift
+++ iFT/iFT/ViewController.swift
@@ -11,53 +11,43 @@ import FirebaseUI
 import Firebase
 
 
-class ViewController: UIViewController {
+// Stage one - Complete sign-in and sign-out / User authentication process
 
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func loginTapped(_ sender: UIButton) {
-    
-    // Get default auth ui
+        // Get default auth ui
         let authUI = FUIAuth.defaultAuthUI()
-        
-        
         // Check that it isn't nil
         guard authUI != nil else {
             // Log the rror
             return
         }
-        
         // Set self as the delegate
         authUI?.delegate = self
         authUI?.providers = [FUIEmailAuth()]
-        
         // Get a reference to the auth UI view controller
         let authViewController = authUI!.authViewController()
-        
         // Show it
         present(authViewController, animated: true, completion: nil)
     }
-        
-        
-    }
     
+    
+}
 
-    
-    extension ViewController: FUIAuthDelegate {
-        
-        func authUI(_ authUI: FUIAuth, didSignInWith authDataResult:
-            AuthDataResult?, error: Error?) {
-            
-            // Check for error
-            guard error == nil else {
-                return
-            }
-            
-            // authDataResult?.user.uid
-            performSegue(withIdentifier: "goHome", sender: self)
+extension ViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult:
+        AuthDataResult?, error: Error?) {
+        guard error == nil else {
+            return
         }
+        performSegue(withIdentifier: "goHome", sender: self)
     }
+}
 
