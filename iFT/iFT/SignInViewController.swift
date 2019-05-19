e5f16a3aae3ca589191e5f527f08061b84cb96b4
Jason <jasonhkendall@gmail.com>
2019-05-19 14:48:46 -0400

    User auth & DB setup
    Spent the afternoon ironing out the navigation controller, completing the user authentication process, sign-in and sign-out process, and fixing some auto layout issues

--- iFT/iFT/SignInViewController.swift
+++ iFT/iFT/SignInViewController.swift
@@ -7,13 +7,49 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
+    
+    // Sign-in form user/pass
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    
+    // VC managing sign in process
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+
         // Do any additional setup after loading the view.
+    }
+    @IBAction func actionSignIn(_ sender: Any) {
+        
+        
+        // Sign In
+        if (txtEmail.text?.isEmpty)! || (txtPassword.text?.isEmpty)! {
+            showAlert(message: "Please provide credentials")
+        } else {
+            Auth.auth().signIn(withEmail: txtEmail.text!, password: txtPassword.text!) { (result, error) in
+                if let err = error {
+                    self.showAlert(message: err.localizedDescription)
+                } else {
+                    print("user authenticated")
+                    // https://coderwall.com/p/cjuzng/swift-instantiate-a-view-controller-using-its-storyboard-name-in-xcode
+                    let controller = self.storyboard?.instantiateViewController(withIdentifier: "authContrllerVerficationID")
+                    self.navigationController?.pushViewController(controller!, animated: true)
+                }
+            }
+        }
+    }
+    
+    // https://www.ioscreator.com/tutorials/display-alert-ios-tutorial
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
 
--- iFT/iFT/SignOutViewController.swift
+++ iFT/iFT/SignOutViewController.swift
@@ -7,24 +7,35 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class SignOutViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func actionSignOut(_ sender: Any) {
+        // Firebase logout API
+        try! Auth.auth().signOut()
+        
+        // **Need code to pop view controllers off the current stack**
+        
+        let ViewController : UIViewController = self.storyboard!.instantiateViewController(withIdentifier: "MainID") as UIViewController
+        UIApplication.shared.keyWindow?.rootViewController = ViewController
     }
-    */
-
 }
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+
