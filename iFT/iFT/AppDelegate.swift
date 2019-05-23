b6c6c0517e6c4528b1084089ec4a475d6917b0e0
Jason <jasonhkendall@gmail.com>
2019-05-23 10:12:36 -0400

    .

--- iFT/iFT/AppDelegate.swift
+++ iFT/iFT/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+       // AppearanceManager.setupAppeearance()
         // Override point for customization after application launch.
         return true
     }
---
+++ iFT/iFT/AppearanceManager.swift
@@ -0,0 +1,23 @@
+//
+//  AppearanceManager.swift
+//  iFT
+//
+//  Created by Jason Howard Kendall on 5/22/19.
+//  Copyright © 2019 JHK. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//class AppearanceManager {
+//    class func setupAppeearance() {
+//      let navBarAppearance = UINavigationBar.appearance()
+//        navBarAppearance.titleTextAttributes =
+//            [NSAttributedString.Key.font: UIFont(name: "Avenir Next", size: 28)!, NSAttributedString.Key.foregroundColor: UIColor.yellow]
+//        
+//        let navbarCompactAppearance = UINavigationBar.appearance(for: UITraitCollection.init(verticalSizeClass: .compact))
+//        navbarCompactAppearance.titleTextAttributes =
+//            [NSAttributedString.Key.font: UIFont(name: "Avenir Next", size: 20)!,
+//             NSAttributedString.Key.foregroundColor: UIColor.orange]
+//    }
+//}
